a557817ba8296283bcf418c6d570270be8dfb210
root <chestervjazmin@gmail.com>
2025-05-20 01:52:18 +0000

    init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+// Global error handler (should be after routes)
+
+export default app;
