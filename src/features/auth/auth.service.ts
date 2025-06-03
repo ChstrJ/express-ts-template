da6319e2564a22d470b07e1525c9beccc3392ce6
root <chestervjazmin@gmail.com>
2025-06-03 04:09:20 +0000

    fixes

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { authRepository } from './auth.repository';
+import { Request } from 'express';
 
 export const authService = {
-  async login(req) {
+  async login(req: Request) {
     const body = req.body;
     const data = await authRepository.findByEmail(body.email)
-    const testDaata = await authRepository.findById('0c106e05-b906-43b2-8bfb-1d46bc793c2b');
     return data;
   }
 }
