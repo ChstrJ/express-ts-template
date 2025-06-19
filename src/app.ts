207626ab1a90c2c44395c1e9d23fa0329ca002a1
root <chestervjazmin@gmail.com>
2025-06-19 01:35:07 +0000

    init

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,26 @@ import cookieParser from 'cookie-parser';
 import apiRoutes from '@routes/api';
 import http from 'http';
 import { join } from 'path';
+import { resolvers, typeDefs } from './graphql-server/schema';
 
-const app = express();
-const server = http.createServer(app);
+async function init() {
+  const app = express();
+  const server = http.createServer(app);
 
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser());
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors());
+  app.use(cookieParser());
 
-// Routes
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+  // Routes
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
 
-app.use('/api', apiRoutes);
+  app.use('/api', apiRoutes);
 
-// Global error handler (should be after routes)
+  // Global error handler (should be after routes)
 
-export default app;
+}
+
+init();
--- src/server.ts
+++ src/server.ts
@@ -8,28 +8,33 @@ import config from './config/config';
 import apiRoutes from '@routes/api';
 import errorHandler from '@middlewares/error-handler';
 import helmet from "helmet";
+import { resolvers, typeDefs } from "./graphql-server/schema";
 
-const app = express();
-const server = http.createServer(app);
+async function init() {
+  const app = express();
+  const server = http.createServer(app);
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(cookieParser());
+  // Middleware
+  app.use(express.json());
+  app.use(cors());
+  app.use(helmet());
+  app.use(cookieParser());
 
-// Routes
-app.get('/', (req, res) => {
-  return res.json({ message: 'It works!' });
-});
+  // Routes
+  app.get('/', (req, res) => {
+    return res.json({ message: 'It works!' });
+  });
 
-app.use('/api', apiRoutes);
+  app.use('/api', apiRoutes);
 
-// Global middleware
-app.use(errorHandler)
+  // Global middleware
+  app.use(errorHandler)
 
-// Start the server
-server.listen(config.app.nodePort, () => {
-  logger.info(`Server running on port ${config.app.nodePort}`);
-});
+  // Start the server
+  server.listen(config.app.nodePort, () => {
+    logger.info(`Server running on port ${config.app.nodePort}`);
+  });
 
+}
+
+init();
