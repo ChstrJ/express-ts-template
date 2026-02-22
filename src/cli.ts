fa527c891d82a9b1b1f25ba859cf74b3884fcd20
Chester <chestervjazmin@gmail.com>
2026-02-22 19:02:19 +0800

    Clean

--- src/cli.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from 'commander';
-import { testCommand } from './commands/test';
-
-const program = new Command();
-
-program.addCommand(testCommand);
-
-program.parse(process.argv);
