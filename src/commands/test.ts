adb1d4fb2d50cdb7a3488e45c4231097ceb587c4
Chester <chestervjazmin@gmail.com>
2026-02-22 19:04:15 +0800

    Clean files

--- src/commands/test.ts
+++
@@ -1,8 +0,0 @@
-// import { Command, program } from 'commander';
-
-// export const testCommand = new Command('test')
-//   .description('A test command that runs a command')
-//   .option('--name <name>', 'Name to greet')
-//   .action((_, options) => {
-//     console.log(options.name)
-//   });
--- src/common/schema/levels.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const levelSchema = z.object({
-  level: z.number().positive().min(1).max(7),
-  percentage: z.coerce.number().positive().min(1).max(50)
-});
-
-export const updateLevelSchema = levelSchema.partial();
